675bedf43b54fc91868d8665959c16c231eba1c3
Emily A Rodriguez <rodriguez.emily1993@gmail.com>
2016-08-18 11:33:34 -0500

    fixed errors

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,3 +1,21 @@
 @extends('layouts.master')
 @section('content')
+	<table class="table table-bordered table-striped table-hover">
+		<thead>
+			<tr>
+				<th>Title</th>
+				<th>URL</th>
+				<th>Content<th>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach($posts as $post)
+				<tr>
+					<td>{{ $post->title }}</td>
+					<td>{{ $post->url }}</td>
+					<td>{{ $post->content }}</td>
+				</tr>
+			@endforeach
+		</tbody>
+	</table>
 @stop
